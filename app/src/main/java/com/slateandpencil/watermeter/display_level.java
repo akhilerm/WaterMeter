161a85d32e03ac1dd2a880f4c8dd129c50e8a71f
Akhil Mohan <akhilerm@gmail.com>
2016-01-13 01:57:46 +0530

    Floating water animation added Blue floating water animation has been added. But the precision of float is limited to 10%. Single  color float

--- app/src/main/java/com/slateandpencil/watermeter/display_level.java
+++ app/src/main/java/com/slateandpencil/watermeter/display_level.java
@@ -17,6 +17,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.Toast;
 import java.util.HashMap;
@@ -25,7 +27,10 @@ import java.util.HashMap;
 public class display_level extends AppCompatActivity {
     MyService mservice;
     HashMap<Integer,Float> m=new HashMap<Integer,Float>();
-
+    final ImageView imageView1 = (ImageView)findViewById(R.id.water_lvl1);
+    final ImageView imageView2 = (ImageView)findViewById(R.id.water_lvl2);
+    final ImageView imageView3 = (ImageView)findViewById(R.id.water_lvl3);
+    final ImageView imageView4 = (ImageView) findViewById(R.id.water_lvl4);
 
     int n;//tank no of the  current tank whose level is to be known
 
@@ -36,7 +41,6 @@ public class display_level extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle(getIntent().getStringExtra("tank_name"));
-        //ImageView imageView=(ImageView)findViewById(R.id.water_lvl);
         n=getIntent().getIntExtra("tank_no",1);
           }
     @Override
@@ -56,16 +60,109 @@ public class display_level extends AppCompatActivity {
 
         }
 
+    public void draw(float lvl){
+        int color = Color.parseColor("#40A4DF");
+        float val=(float)(0.86*(1-lvl));
+        imageView1.setColorFilter(color);
+        imageView2.setColorFilter(color);
+        imageView3.setColorFilter(color);
+        imageView4.setColorFilter(color);
+        //Animation for upward movement of water
+        Animation animation1 = new TranslateAnimation(Animation.RELATIVE_TO_SELF, -1f, Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, 0.86f, Animation.RELATIVE_TO_SELF, val);
+        Animation animation2 = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, 1f, Animation.RELATIVE_TO_SELF, 0.86f, Animation.RELATIVE_TO_SELF, val);
+        Animation animation3 = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 1f, Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, 0.86f, Animation.RELATIVE_TO_SELF, val);
+        Animation animation4 = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, -1f, Animation.RELATIVE_TO_SELF, 0.86f, Animation.RELATIVE_TO_SELF, val);
+        animation1.setDuration(5300);
+        animation2.setDuration(5300);
+        animation3.setDuration(5100);
+        animation4.setDuration(5100);
+        animation1.setFillAfter(true);
+        animation2.setFillAfter(true);
+        animation3.setFillAfter(true);
+        animation4.setFillAfter(true);
+        imageView1.startAnimation(animation1);
+        imageView2.startAnimation(animation2);
+        imageView3.startAnimation(animation3);
+        imageView4.startAnimation(animation4);
+        //Animation for wave movement of water
+        switch((int)(lvl*10)){
+            case 1:
+                val+=0.085;
+                break;
+            case 2:
+                val+=0.075;
+                break;
+            case 3:
+                val+=0.065;
+                break;
+            case 4:
+                val+=0.055;
+                break;
+            case 5:
+                val+=0.047;
+                break;
+            case 6:
+                val+=0.037;
+                break;
+            case 7:
+                val+=0.027;
+                break;
+            case 8:
+                val+=0.018;
+                break;
+            case 9:
+                val+=0.008;
+                break;
+        }
+        final Animation animation5 = new TranslateAnimation(Animation.RELATIVE_TO_SELF, -1f, Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, val, Animation.RELATIVE_TO_SELF, val);
+        final Animation animation6 = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, 1f, Animation.RELATIVE_TO_SELF, val, Animation.RELATIVE_TO_SELF, val);
+        final Animation animation7 = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 1f, Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, val, Animation.RELATIVE_TO_SELF, val);
+        final Animation animation8 = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, -1f, Animation.RELATIVE_TO_SELF, val, Animation.RELATIVE_TO_SELF, val);
+        animation5.setRepeatCount(Animation.INFINITE);
+        animation6.setRepeatCount(Animation.INFINITE);
+        animation7.setRepeatCount(Animation.INFINITE);
+        animation8.setRepeatCount(Animation.INFINITE);
+        animation5.setFillAfter(true);
+        animation6.setFillAfter(true);
+        animation7.setFillAfter(true);
+        animation8.setFillAfter(true);
+        animation5.setDuration(5300);
+        animation6.setDuration(5300);
+        animation7.setDuration(4100);
+        animation8.setDuration(4100);
+        //Animation Listener
+        animation1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView1.startAnimation(animation5);
+                imageView2.startAnimation(animation6);
+                imageView3.startAnimation(animation7);
+                imageView4.startAnimation(animation8);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        //Wave animation ends here
+    }
+
     public void show()  {
 
             m = (HashMap) mservice.display();
 
             float lvl;
             lvl = (float) m.get(n);
-            Log.e("varunnilla",""+lvl);
-            Toast.makeText(getApplicationContext(), ""+lvl, Toast.LENGTH_LONG).show();
-
-
+            Log.e("varunnilla", "" + lvl);
+            draw(lvl);
 
         }
 
